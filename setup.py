cc62dad613f26bc9e6626da2e529779a7ee1f196
Brian Hines <brian@projectweekend.net>
2015-12-11 13:52:05 -0600

    More info

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from setuptools import setup
 setup(
     name='pg-table-markdown',
     version='1.0.0',
+    author='Vokal',
+    author_email='pypi@vokal.io',
+    description='A command line tool that generates markdown documentation for Postgres tables in a given schema',
     packages=['pg_table_markdown'],
     py_modules=['pg_table_markdown'],
     install_requires=[
