0acf31d25b496a613bf8a4e5e1bf63c81b15fe33
Brian Hines <brian@projectweekend.net>
2015-12-11 14:01:31 -0600

    add url

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author='Vokal',
     author_email='pypi@vokal.io',
     description='A command line tool that generates markdown documentation for Postgres tables in a given schema',
+    url='https://github.com/vokal/pg-table-markdown',
     packages=['pg_table_markdown'],
     py_modules=['pg_table_markdown'],
     install_requires=[
