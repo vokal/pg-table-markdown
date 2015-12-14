4d292383f0b2c79f6b06db16589d93231cfc8cb2
Brian Hines <brian@projectweekend.net>
2015-12-14 08:38:58 -0600

    Sort results
    Table names in alpha order, columns by ordinal_position

--- pg_table_markdown/queries.py
+++ pg_table_markdown/queries.py
@@ -9,7 +9,8 @@ def build_schema_query(table_schema):
                     character_maximum_length
         FROM        information_schema.columns
         WHERE       table_schema = '{0}'
-        ORDER BY    table_name
+        ORDER BY    table_name,
+                    ordinal_position
     )
     SELECT      JSON_AGG(table_schema_info.*)
     FROM        table_schema_info;
