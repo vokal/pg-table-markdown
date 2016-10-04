c969accb4783896e3225d126e23e0c6d5851de7f
cssho <rizeupbass@gmail.com>
2016-10-04 13:10:25 +0900

    Support display maximum length of character varying.

--- pg_table_markdown/__init__.py
+++ pg_table_markdown/__init__.py
@@ -9,13 +9,14 @@ SECTION_HEADING = '### {0} \n\n'
 TABLE_HEADER = 'Column | Type | Default | Nullable \n'
 TABLE_DIVIDER = '--- | --- | --- | --- \n'
 TABLE_ROW = '{column_name} | {data_type} | {column_default} | {is_nullable} \n'
-
+TABLE_ROW_WITH_MAXLENGTH = '{column_name} | {data_type}({character_maximum_length}) | {column_default} | {is_nullable} \n'
 
 @click.command()
 @click.option('--database_url', prompt=True, help='Database connection URL')
 @click.option('--table_schema', default='public', help='Postgres table_schema, default is: public')
 @click.option('--output_file', prompt=True, help='Path for generated markdown file')
-def cli(database_url, table_schema, output_file):
+@click.option('--max_length', is_flag=True, help='To display maximum length of character varying, default is: False')
+def cli(database_url, table_schema, output_file, max_length):
     """
     Export Postgres table documentation to a markdown file
     """
@@ -37,5 +38,8 @@ def cli(database_url, table_schema, output_file):
             f.write(TABLE_HEADER)
             f.write(TABLE_DIVIDER)
             for column in parsed[table_name]:
-                f.write(TABLE_ROW.format(**column))
+                if max_length and column['character_maximum_length'] is not None: 
+                    f.write(TABLE_ROW_WITH_MAXLENGTH.format(**column))
+                else:
+                    f.write(TABLE_ROW.format(**column))
             f.write('\n')
